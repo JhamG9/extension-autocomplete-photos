3babfb12898b4dd2c2e9b70fa1cb91d7376f9325
Jose Jhampier Giron <jgiron@bch.bancodechile.cl>
2024-11-25 11:53:53 -0500

    feat: added initial commit and load values on input

---
+++ popup.js
@@ -0,0 +1,18 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   const targetElement = document.querySelector('body');
+
+//   // Ver el nodo del elemento en la consola
+//   console.log(targetElement);
+
+//   // Mostrar el contenido del body como un string en un alert
+//   alert(targetElement.innerHTML);  // Esto traerá todo el HTML dentro del body
+
+//   // También puedes mostrar solo el texto sin etiquetas HTML
+//   // alert(targetElement.textContent);
+
+//   if (targetElement) {
+//     console.log("Elemento encontrado:", targetElement.textContent);  // Imprime el texto del body
+//   } else {
+//     console.log("No se encontró el elemento.");
+//   }
+// });
